8e420a3194129ac9788021fe3668462b10c704ae
Samiha Hossain <75538711+Samiha-Hossain19@users.noreply.github.com>
2022-11-27 00:56:52 +0600

    initial commit

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Todos from './Todos'
 
 import style from './home.module.css'
+import NewTodo from './NewTodo'
 
 const dummyTodos =[
     {
@@ -20,6 +21,7 @@ const Home = () => {
   return (
     <div className={style.container}>
       <h1 style={{color : "white"}}>ToDo App</h1>
+      <NewTodo />
         < Todos todos ={dummyTodos} />
     </div>
   )
